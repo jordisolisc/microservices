95b1ce36e9fc4b30465aa6d0231681c567d0950c
SOLIS CALANCHA Jordi <jordi.solis-calancha@ext.ec.europa.eu>
2021-01-16 17:58:51 +0100

    Create Department Service

---
+++ departmentService/src/main/java/com/njesoft/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.njesoft.department.repository;
+
+import com.njesoft.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
