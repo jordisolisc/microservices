fe7406dc5bc3ebbeb8de429f80097de453529f8e
SOLIS CALANCHA Jordi <jordi.solis-calancha@ext.ec.europa.eu>
2021-01-23 20:39:46 +0100

    Create FallBack Methods for User and Deparment Service

---
+++ cloud-gateway/src/main/java/com/njesoft/cloud/gateway/FallBackMethodController.java
@@ -0,0 +1,20 @@
+package com.njesoft.cloud.gateway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod(){
+        return "User Services is taking longer than expected." +
+                "Please try again later.";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod(){
+        return "Department Services is taking longer than expected." +
+                "Please try again later.";
+    }
+}
